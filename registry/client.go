eda88e215937dbf0c2e4687275453fb5144d6b21
Mohammed Hamza <gh3173@wayne.edu>
2021-10-07 23:01:31 -0400

    service registration

---
+++ registry/client.go
@@ -0,0 +1,39 @@
+package registry
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func RegisterService(r Registration) error {
+	buf := new(bytes.Buffer)
+	enc := json.NewEncoder(buf)
+	err := enc.Encode(r)
+	if err != nil {
+		return err
+	}
+
+	res, err := http.Post(ServiceURL, "application/json", buf)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("Failed to register service. Registry service responded with code %v", res.StatusCode)
+	}
+	return nil
+}
+
+func ShutDownService(serviceURL string) error {
+	req, err := http.NewRequest(http.MethodDelete, ServiceURL, bytes.NewBuffer([]byte(serviceURL)))
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "text/plain")
+	res, err := http.DefaultClient.Do(req)
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("Failed to deregister service. Registry service responded with code %v", res.StatusCode)
+	}
+	return err
+}
